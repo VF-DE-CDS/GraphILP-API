5a068382c584cc8e7ed1248d76baaa6da4be966b
Thomas S <tcsauter@web.de>
2021-01-28 09:39:26 +0100

    Added Weighted Vertex Cover Problem - still needs working on to make it work with testing instances.

--- covering/min_vertexcover.py
+++ covering/min_vertexcover.py
@@ -3,7 +3,7 @@ from gurobipy import *
 
 def createModel(G):    
     """ Create an ILP for the minimum vertex cover problem                
-    Arguments:            G -- a weighted ILPGraph                    
+    Arguments:            G -- an unweighted ILPGraph                    
     Returns:            a Gurobi model     
     """        
     # Create model    
@@ -29,3 +29,27 @@ def extractSolution(G, model):
     vertex_nodes = [node for node, node_var in G.node_variables.items() if node_var.X > 0.5]        
     
     return vertex_nodes
+
+def createModelWeighted(G):
+    """ Create an ILP for the minimum vertex cover problem                
+    Arguments:              G -- a weighted ILPGraph                    
+    Returns:                a Gurobi model     
+    """        
+    # Create model    
+    m = Model("graphilp_min_vertex_cover")        
+    # Add variables for edges and nodes    
+    G.setNodeVars(m.addVars(G.G.nodes(), vtype=gurobipy.GRB.BINARY))    
+    node_list = G.G.nodes()
+    weights = []
+    for node in node_list(data=True):
+        weights.append(node[1]['prize'])
+    m.update()    
+    nodes = G.node_variables        
+    # Create constraints    
+    ## for every edge, at least one vertex must be in a vertex cover of G    
+    for (u, v) in G.G.edges:                
+        m.addConstr(nodes[u] + nodes[v] >= 1 )    
+    # set optimisation objective: minimize cardinality of the vertex cover   
+    m.setObjective( gurobipy.quicksum(nodes*weights), GRB.MINIMIZE)      
+
+    return m
