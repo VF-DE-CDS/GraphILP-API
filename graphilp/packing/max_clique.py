828af87004913501459b2d1f4a86aa5a2f637248
Rolf Bardeli <rolf.bardeli1@vodafone.com>
2021-02-10 12:12:21 +0100

    docs for packing

--- graphilp/packing/max_clique.py
+++ graphilp/packing/max_clique.py
@@ -4,11 +4,9 @@ from itertools import combinations
 def createModel(G):
     """ Create an ILP for the maximum clique problem
         
-        Arguments:
-            G -- a weighted ILPGraph
-            
-        Returns:
-            a Gurobi model 
+    :param G: a weighted bipartite ILPGraph
+
+    :return: a `gurobipy model <https://www.gurobi.com/documentation/9.1/refman/py_model.html>`_
     """
     
     # Create model
@@ -34,12 +32,10 @@ def createModel(G):
 def extractSolution(G, model):
     """ Get a list of vertices comprising a maximal clique
     
-        Arguments:
-            G     -- a weighted ILPGraph
-            model -- a solved Gurobi model for maximum clique
-            
-        Returns:
-            a list of vertices comprising a maximal clique
+    :param G: a weighted ILPGraph
+    :param model: a solved Gurobi model for maximum clique
+
+    :return: a list of vertices comprising a maximal clique
     """
     clique_nodes = [node for node, node_var in G.node_variables.items() if node_var.X > 0.5]
     
