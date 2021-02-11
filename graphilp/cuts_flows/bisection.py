7e181bd8750853e666ce7afdc491d07d684eecda
Rolf Bardeli <rolf.bardeli1@vodafone.com>
2021-02-11 17:38:12 +0100

    bisection docs

--- graphilp/cuts_flows/bisection.py
+++ graphilp/cuts_flows/bisection.py
@@ -3,12 +3,25 @@ import math
 
 
 def createModel(G, direction=GRB.MAXIMIZE):    
-    """ Create an ILP for the minimum/maximum bisection problem
+    r""" Create an ILP for the minimum/maximum bisection problem
     
     :param G: an ILPGraph                    
     :param direction: GRB.MAXIMIZE for maximum weight matching, GRB.MINIMIZE for minimum weight matching
     
     :return: a `gurobipy model <https://www.gurobi.com/documentation/9.1/refman/py_model.html>`_
+    
+    ILP:
+        .. math::
+            :nowrap:
+            
+            \begin{align*}
+            & \min / \max_{\{u,v\} \in E} \sum x_{u,v}\\
+            & \text{s.t.} &\\
+            & \sum_{v \in V} x_v \leq \lceil |V|/2 \rceil & \text{(lower bound size of one partition)}\\
+            & \sum_{v \in V} x_v \geq \lfloor |V|/2 \rfloor & \text{(upper bound size of one partition)}\\
+            & \forall \{u,v\} \in E: x_{u, v} \leq x_u + x_v & \text{(no edge between partitions)}\\
+            & \forall \{u,v\} \in E: x_{u, v} \leq 2 - x_u - x_v & \text{(no edge between partitions)}\\
+            \end{align*}    
     """        
     # Create model    
     m = Model("graphilp_bisection")  
@@ -22,21 +35,21 @@ def createModel(G, direction=GRB.MAXIMIZE):
     nodes = G.node_variables
     
     number_nodes = len(G.G.nodes())
-    bound_upper = math.ceil( number_nodes/2 )
-    bound_lower = math.floor( number_nodes/2 )
+    bound_upper = math.ceil(number_nodes/2)
+    bound_lower = math.floor(number_nodes/2)
 
     # Create constraints
     # balanced solutions are needed
-    m.addConstr( gurobipy.quicksum(nodes) <= bound_upper )
-    m.addConstr( gurobipy.quicksum(nodes) >= bound_lower )
+    m.addConstr(gurobipy.quicksum(nodes) <= bound_upper)
+    m.addConstr(gurobipy.quicksum(nodes) >= bound_lower)
 
     ## for every edge, the nodes must be separated    
     for (u, v) in G.G.edges():                
-        m.addConstr( edges[(u, v)]  <= nodes[v] +nodes[u] )
-        m.addConstr( edges[(u, v)] <= 2 - nodes[v] - nodes[u] ) 
+        m.addConstr(edges[(u, v)] <= nodes[v] + nodes[u])
+        m.addConstr(edges[(u, v)] <= 2 - nodes[v] - nodes[u]) 
    
     # set optimisation objective: minimize/maximize the cardinality of the number of edges in the cut   
-    m.setObjective( gurobipy.quicksum(  edges), direction)   
+    m.setObjective(gurobipy.quicksum(edges), direction)   
 
     return m
 
