b99fb4ea2f961fa56931922aedaacc03e37f82cc
Thomas S <tcsauter@web.de>
2021-02-16 16:53:38 +0100

    Added Mixed Steiner Flow Formulation with Labeling Constraints

--- graphilp/network/Steiner_Flow_withLinear.py
+++ graphilp/network/Steiner_Flow_withLinear.py
@@ -1,8 +1,9 @@
 # +
-from gurobipy import *
 import networkx as nx
+from gurobipy import *
 
-def createModel(G, terminals = [1,2], weight = 'Cost', root = 1):    
+
+def createModel(G, terminals = [1,2], weight = 'weight', root = 1):    
     r""" Create an ILP for the linear Steiner Problem. 
     
     The model can be seen in Paper Chapter 3.0. This model
@@ -35,22 +36,13 @@ def createModel(G, terminals = [1,2], weight = 'Cost', root = 1):
         G.G = nx.DiGraph(G.G)
     
     Terminals = len(terminals)
-    Capacity = 2000
+    n = len(G.G.nodes())
+    Capacity = Terminals
     
     edges = G.G.edges()
-
-    # Create Neighbourhood of the Root
-    Neighbourhood = []
-    for edge in G.G.edges():
-        if edge[0] == root or edge[1] == root:
-            Neighbourhood.append(edge)
     
     # create model
     m = Model("Steiner Tree - Flow Formulation")        
-    source = 0
-    
-    for terminal in terminals:
-        G.G.add_edge(source, terminal, Cost = 0)
     
     # create reverse edge for every edge in the graph
     for edge in G.G.edges(data=True):
@@ -59,41 +51,64 @@ def createModel(G, terminals = [1,2], weight = 'Cost', root = 1):
         else:
             G.G.add_edge(edge[1], edge[0], Cost = edge[2]['Cost'])
     
+    # Create Neighbourhood of the Root
+    Neighbourhood = []
+    for edge in G.G.edges():
+        if edge[0] == root:
+            Neighbourhood.append(edge)
+
+    print(Neighbourhood)
     # Edge variables y
     y = {}
     f = {}
+
     # Label variable x
+    x = {}
+    l = {}
+    
     for edge in G.G.edges():
-        y[edge] = m.addVar(vtype = gurobipy.GRB.BINARY, name = "y" + str(edge))
+        # Variable that determiens whether edge is chosen
+        y[edge] = m.addVar(vtype = GRB.BINARY, name = "y" + str(edge))
+        
+        # Variable the determines the flow amount on the edge
         f[edge] = m.addVar(name = "f" + str(edge), lb = 0, ub = Capacity)
-    #G.setEdgeVars(m.addVars(G.G.edges(), vtype = gurobipy.GRB.BINARY))
-    # Flow Variables z
-    #G.setFlowVars(m.addVars(G.G.edges(), vtype = gurobipy.GRB.INTEGER, name = "z"))
+
+    for node in G.G.nodes():
+        # Label Variable of the Node
+        l[node] = m.addVar(vtype = gurobipy.GRB.INTEGER, lb = 0, ub = n)
+
+        # Decision Variable that specifies whether this node is chosen
+        x[node] = m.addVar(vtype = gurobipy.GRB.BINARY)
     m.update()  
 
     # abbreviations
     edges = G.G.edges()
     nodes = G.G.nodes()
-    edge2var = dict(zip(edges.keys(), edges.values()))
     
-    # set objective: minimise the sum of the weights of edges selected for the solution
-    m.setObjective(gurobipy.quicksum([y[edge] * 1 for edge in edges]), GRB.MINIMIZE)
+    # set objective: minimise the sum of the costs of edges selected for the solution
+    m.setObjective(gurobipy.quicksum([y[edge] * G.G.edges[edge][weight] for edge in edges]), GRB.MINIMIZE)
 
     for edge in edges:
         # at most one direction per edge can be chosen
         # runtime can probably be greatly improved if iterating is done in a smarter way
         reverseEdge = edge[::-1]
         m.addConstr(y[edge] + y[reverseEdge] <= 1)
+        
         # Maximum Capacity must not be exceeded for every edges
         m.addConstr(f[edge] <= y[edge] * Capacity)
-    
-    # All flow has to end in the Root
-    m.addConstr(gurobipy.quicksum(f[edge] for edge in Neighbourhood) == Terminals - 1)   
+        
+        # Labeling constraints
+        m.addConstr( n * y[reverseEdge] + l[edge[1]] - l[edge[0]] >= 1 - n*(1 - y[edge]))
+        m.addConstr( n * y[edge] + l[edge[0]] - l[edge[1]] >= 1 - n*(1 - y[reverseEdge]))
+        
+        # If edge is chosen, both Nodes need to be chosen
+        m.addConstr(2*y[edge] - x[edge[0]] -x[edge[1]] <= 0)
 
     # Flow conservation constraint
     for j in nodes:
         incomingEdges = []
         outgoingEdges = []
+        # Searching for all incoming and outgoing Edges from and into the Node
         for i in nodes:
             if (i == j):
                 continue
@@ -101,19 +116,20 @@ def createModel(G, terminals = [1,2], weight = 'Cost', root = 1):
                 incomingEdges.append((i,j))
             if((j, i) in edges):
                 outgoingEdges.append((j,i))
-        if j != root and j != 0:
+        # If a node that is not the Root, Source or a Terminal is found, no flow must be lost
+        if j != root and (j not in terminals):
             m.addConstr(sum(f[edgeOut] for edgeOut in outgoingEdges) - sum(f[edgeIn] for edgeIn in incomingEdges) == 0)
+        # Else if a terminal was found, it has to consume a flow of 1
+        elif (j in terminals and j != root):
+            m.addConstr(sum(f[edgeIn] for edgeIn in incomingEdges) - sum(f[edgeOut] for edgeOut in outgoingEdges)== 1)
 
-    # Flow is started from Source node and must not be returned to Source Node
+    # Flow is started from Root node. Outgoing Flow has to be enough to fill all nodes
+    m.addConstr(gurobipy.quicksum(f[edge] for edge in Neighbourhood) == Terminals - 1)   
+    
+    # Flow is started from Root node and must not be returned to Source Node
     for t in terminals:
-        if t == root:
-            m.addConstr(f[0, t] == 0)
-            m.addConstr(f[t, 0] == 0)
-            continue
-        m.addConstr(f[0, t] == 1)
-        m.addConstr(f[t, 0] == 0)
-        m.addConstr(y[0, t] == 1)
-        m.addConstr(y[t, 0] == 0)
+        m.addConstr(x[t] == 1)
+    
     #print(m.computeIIS())
     m.optimize() 
     for edge in edges:
