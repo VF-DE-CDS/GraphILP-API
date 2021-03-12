71ff6f79d0e2fb894dc7e2570ae0b2f026640e4b
Rolf Bardeli <rolf.bardeli1@vodafone.com>
2021-03-12 08:05:10 +0100

    docs for vertex cover warmstarts

--- graphilp/covering/warmstart_vertexcover.py
+++ graphilp/covering/warmstart_vertexcover.py
@@ -4,6 +4,9 @@ from gurobipy import *
 def maximalMatching(G):
     """
     Implements the maximal matching heuristic
+    
+    This heuristic successively chooses an edge in the graph, adds its vertices to the vertex cover
+    and then deletes all edges adjacent to these vertices because they are already covered.
 
     :param G: an :py:class:`~graphilp.imports.ilpgraph.ILPGraph` 
     
@@ -13,14 +16,17 @@ def maximalMatching(G):
     warmstart = []
     G_copy = G.G.copy()
     
+    # successively delete covered edges until all edges are covered
     while G_copy.number_of_edges() > 0:
+        # choose an edge
         edge = list(G_copy.edges())[0]
         
+        # add vertices of the edge to vertex cover
         for v in edge:
             warmstart.append(v)
-            
+        
+        # remove all edges adjacent to the chosen edge
         remove_edges = list(G_copy.edges(edge))
-        print("removing " + str(remove_edges))
         G_copy.remove_edges_from(remove_edges)
             
             
