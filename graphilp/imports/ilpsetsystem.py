4aebd9520d28fbba2caa6ecce15ba1fe545c07e0
Rolf Bardeli <rolf.bardeli1@vodafone.com>
2021-03-19 13:17:12 +0100

    docs for ilpsetsystem

--- graphilp/imports/ilpsetsystem.py
+++ graphilp/imports/ilpsetsystem.py
@@ -1,16 +1,42 @@
 class ILPSetSystem:        
+    """ Wrapper class for set systems (undirected hyper graphs)
     
-    def setUniverse(self, U):        
+    Joint representation of set system instances and variables of a related integer linear program
+    """
+    
+    def setUniverse(self, U):  
+        """ set the universe of the set system
+        
+        :param U: the universe of the set system
+        """
         self.U = U
     
-    def setSystem(self, S):        
+    def setSystem(self, S):  
+        """ set the names of the sets in the system
+        
+        :param S: the system of the set system
+        """
         self.S = S
     
-    def setIncMatrix(self, M):        
+    def setIncMatrix(self, M):    
+        """ set the incidence matrix of the system
+        
+        The incidence matrix indicates which element of the universe is contained in which set of the system.
+        
+        :param M: the incidence matrix of the set system
+        """
         self.M = M
     
-    def setSystemVars(self, variables):        
+    def setSystemVars(self, variables):  
+        """ set the dictionary of indicator variables for the elements of the system
+                
+        :param variables: a dictionary with variable names as keys and gurobipy variables as values
+        """
         self.system_variables = variables            
     
-    def setUniverseVars(self, variables):        
+    def setUniverseVars(self, variables):      
+        """ set the dictionary of indicator variables for elements of the universe
+
+        :param variables: a dictionary with variable names as keys and gurobipy variables as values
+        """
         self.universe_variables = variables
