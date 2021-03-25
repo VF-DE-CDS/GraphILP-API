80060aac8e2c5a86f38b16d912b135eea6449fda
Rolf Bardeli <rolf.bardeli1@vodafone.com>
2021-03-25 16:50:46 +0100

    corrected docstrings

--- graphilp/covering/k_cover.py
+++ graphilp/covering/k_cover.py
@@ -3,7 +3,7 @@ from gurobipy import *
 import numpy as np
 
 def createModel(S, A, k):
-    """ Greate an ILP for the k-coverage problem
+    r""" Greate an ILP for the k-coverage problem
     
     :param S: a weighted :py:class:`~graphilp.imports.ilpsetsystem.ILPSetSystem`
     :param A: Sparse covering matrix compressed by rows. Rows are Nodes and Columns are the Sets covering the Nodes 
@@ -12,17 +12,17 @@ def createModel(S, A, k):
     :return: a `gurobipy model <https://www.gurobi.com/documentation/9.1/refman/py_model.html>`_
     
     ILP:
-    
         .. math::
             :nowrap:
-                \begin{align*}
-                \min \sum_{s \in S} x_{s} \\
-                \text{s.t.} &&\\
-                \forall e \in U: \sum_{S:e \in S}x_{s} \geq 1 && \text{(cover every element of the universe)}\\
-                \forall s \in S: x_{s} \in \{0,1\} && \text{(exactly one outgoing edge)}\\
-                \sum_{(s, v) \in E}x_{sv} = 1 && \text{(every set is either in the set cover (1), otherwise 0)}\\
-                \sum_{s \in S} x_{s} \leq k && \text{(Use maximum k sets)}\\
-                \end{align*}
+            
+            \begin{align*}
+            \min \sum_{s \in S} x_{s} \\
+            \text{s.t.} &&\\
+            \forall e \in U: \sum_{S:e \in S}x_{s} \geq 1 && \text{(cover every element of the universe)}\\
+            \forall s \in S: x_{s} \in \{0,1\} && \text{(exactly one outgoing edge)}\\
+            \sum_{(s, v) \in E}x_{sv} = 1 && \text{(every set is either in the set cover (1), otherwise 0)}\\
+            \sum_{s \in S} x_{s} \leq k && \text{(Use maximum k sets)}\\
+            \end{align*}
     """
     
     # Create model
--- graphilp/covering/set_cover.py
+++ graphilp/covering/set_cover.py
@@ -14,13 +14,14 @@ def createModel(S, A):
     ILP:
         .. math::
             :nowrap:
-                \begin{align*}
-                \min \sum_{s \in S} x_{s} \\
-                \text{s.t.} &&\\
-                \forall e \in E: \sum_{S:e \in S}x_{s} \leq 1 && \text{(cover every element of the universe)}\\
-                \forall s \in S: x_{s} \in \{0,1\} && \text{(exactly one outgoing edge)}\\
-                \sum_{(s, v) \in E}x_{sv} = 1 && \text{(every set is either in the set cover (1), otherwise 0)}\\
-                \end{align*}
+            
+            \begin{align*}
+            \min \sum_{s \in S} x_{s} \\
+            \text{s.t.} &&\\
+            \forall e \in E: \sum_{S:e \in S}x_{s} \leq 1 && \text{(cover every element of the universe)}\\
+            \forall s \in S: x_{s} \in \{0,1\} && \text{(exactly one outgoing edge)}\\
+            \sum_{(s, v) \in E}x_{sv} = 1 && \text{(every set is either in the set cover (1), otherwise 0)}\\
+            \end{align*}
     """
     
     # Create model
