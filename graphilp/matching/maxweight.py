07cfdea93a75cbf8684073d9fd0bc4c62853d1fd
Rolf Bardeli <rolf.bardeli1@vodafone.com>
2021-03-30 15:46:22 +0200

    PEPed

--- graphilp/matching/maxweight.py
+++ graphilp/matching/maxweight.py
@@ -1,13 +1,14 @@
-from gurobipy import *
+from gurobipy import Model, GRB, quicksum
 
-def createModel(G, weight='weight'):
+
+def create_model(G, weight='weight'):
     r""" Create an ILP for maximum weight matching
-        
-    :param G: a weighted :py:class:`~graphilp.imports.ilpgraph.ILPGraph` 
+
+    :param G: a weighted :py:class:`~graphilp.imports.ilpgraph.ILPGraph`
     :param weight: name of the weight parameter in the edge dictionary of the graph
-    
+
     :return: a `gurobipy model <https://www.gurobi.com/documentation/9.1/refman/py_model.html>`_
-    
+
     ILP:
         .. math::
             :nowrap:
@@ -15,51 +16,50 @@ def createModel(G, weight='weight'):
             \begin{align*}
             \max \sum_{\{i, j\} \in E} w_{ij} x_{ij}\\
             \text{s.t.} &&\\
-            \forall \{u, v\} \in E: x_{uv} - x_u \leq 0 && 
+            \forall \{u, v\} \in E: x_{uv} - x_u \leq 0 &&
             \text{(choosing an edge implies choosing both of its vertices)}\\
-            \forall \{u, v\} \in E: x_{uv} - x_v \leq 0 && 
+            \forall \{u, v\} \in E: x_{uv} - x_v \leq 0 &&
             \text{(choosing an edge implies choosing both of its vertices)}\\
-            \forall v \in V: \sum_{\{u,v\} \in E} x_{uv} \leq 1 && 
+            \forall v \in V: \sum_{\{u,v\} \in E} x_{uv} \leq 1 &&
             \text{(at most one edge adjacent to each vertex)}\\
-            \end{align*} 
+            \end{align*}
     """
-    
     # Create model
     m = Model("graphilp_max_weight_matching")
-    
+
     # Add variables for edges and nodes
-    G.setEdgeVars(m.addVars(G.G.edges(), vtype=gurobipy.GRB.BINARY))
-    G.setNodeVars(m.addVars(G.G.nodes(), vtype=gurobipy.GRB.BINARY))
+    G.set_edge_vars(m.addVars(G.G.edges(), vtype=GRB.BINARY))
+    G.set_node_vars(m.addVars(G.G.nodes(), vtype=GRB.BINARY))
     m.update()
 
     edges = G.edge_variables
     nodes = G.node_variables
-    
+
     # Create constraints
-    ## choosing an edge implies choosing both of its vertices
+    # choosing an edge implies choosing both of its vertices
     for edge, edge_var in edges.items():
         m.addConstr(edge_var - nodes[edge[0]] <= 0)
         m.addConstr(edge_var - nodes[edge[1]] <= 0)
 
-    ## at most one edge can be adjacent to each vertex
+    # at most one edge can be adjacent to each vertex
     for node in nodes:
-        m.addConstr(quicksum([edge_var for edge, edge_var in edges.items() if edge[0]==node or edge[1]==node]) <= 1)     
-        
+        m.addConstr(quicksum([edge_var for edge, edge_var in edges.items() if edge[0] == node or edge[1] == node]) <= 1)
+
     # set optimisation objective: maximum weight matching (sum of weights of chosen edges)
     m.setObjective(quicksum([edge_var * G.G.edges[edge].get(weight, 1) for edge, edge_var in edges.items()]),
                    GRB.MAXIMIZE)
-    
+
     return m
 
-def extractSolution(G, model):
+
+def extract_solution(G, model):
     """ Get a list of the edges comprising the maximum weight matching
-    
+
         :param G: a weighted :py:class:`~graphilp.imports.ilpgraph.ILPGraph`
         :param model: a solved Gurobi model for maximum weight matching
-            
+
         :return: a list of edges comprising the maximum weight matching
     """
     matching = [edge for edge, edge_var in G.edge_variables.items() if edge_var.X > 0.5]
-    
-    return matching
 
+    return matching
--- graphilp/matching/perfect_bipartite.py
+++ graphilp/matching/perfect_bipartite.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from gurobipy import *
+from gurobipy import Model, GRB, quicksum
 
-def createModel(G, A, weight='weight', direction=GRB.MAXIMIZE):
+
+def create_model(G, A, weight='weight', direction=GRB.MAXIMIZE):
     r""" Create an ILP for maximum/minimum bipartite perfect matching
-        
+
     :param G: a weighted bipartite :py:class:`~graphilp.imports.ilpgraph.ILPGraph`
     :param A: subset of the vertex set of G giving the bipartition (each edge has exactly one end in A)
     :param weight: name of the weight parameter in the edge dictionary of the graph
@@ -11,18 +12,18 @@ def createModel(G, A, weight='weight', direction=GRB.MAXIMIZE):
 
     :return: a `gurobipy model <https://www.gurobi.com/documentation/9.1/refman/py_model.html>`_
 
-    ILP:    
+    ILP:
         .. math::
             :nowrap:
 
             \begin{align*}
             \max / \min \sum_{\{i, j\} \in E} w_{ij} x_{ij}\\
             \text{s.t.} &&\\
-            \forall u \in A: \sum_{\{u,v\} \in E} x_{uv} = 1 && 
+            \forall u \in A: \sum_{\{u,v\} \in E} x_{uv} = 1 &&
             \text{(exactly one edge adjacent to each } u \in A\text{)}\\
-            \forall u \in V \setminus A: \sum_{\{u,v\} \in E} x_{uv} = 1 && 
+            \forall u \in V \setminus A: \sum_{\{u,v\} \in E} x_{uv} = 1 &&
             \text{(exactly one edge adjacent to each } u \in V \setminus A\text{)}\\
-            \end{align*} 
+            \end{align*}
 
         This is actually a linear program, i.e., solutions to the LP relaxation are automatically integral.
 
@@ -30,58 +31,57 @@ def createModel(G, A, weight='weight', direction=GRB.MAXIMIZE):
         Lovász, Plummer: `Matching Theory <https://www.ams.org/publications/authors/books/postpub/chel-367>`__, Chapter 7.1.
 
     Example:
-        .. list-table:: 
+        .. list-table::
            :widths: 50 50
            :header-rows: 0
 
            * - .. image:: images/example_bipartite.png
              - `Two-coloured partitions <https://github.com/VF-DE-CDS/GraphILP-API/blob/develop/graphilp/examples/2ColouredPartitions.ipynb>`_
 
-               Learn how to use perfect matching in bipartite graphs to find a way 
+               Learn how to use perfect matching in bipartite graphs to find a way
 
-               to connect n random blue points in the plane 
+               to connect n random blue points in the plane
 
                to n random orange points without crossings.
     """
-    
+
     # Create model
     m = Model("graphilp_bipartite_perfect_matching")
-    
+
     # Add variables for edges and nodes
-    G.setEdgeVars(m.addVars(G.G.edges(), vtype=gurobipy.GRB.BINARY))
+    G.set_edge_vars(m.addVars(G.G.edges(), vtype=GRB.BINARY))
     m.update()
 
     edges = G.edge_variables
     nodes = G.G.nodes()
-    
+
     # Create constraints
-    ## for each u in A there is exactly one edge adjacent to it
+    # for each u in A there is exactly one edge adjacent to it
     for u in A:
-        m.addConstr(gurobipy.quicksum([edge_var for edge, edge_var in edges.items() if u in edge]) == 1)
-    
-    ## for each v in V setminus A there is exactly one edge adjacent to it
+        m.addConstr(quicksum([edge_var for edge, edge_var in edges.items() if u in edge]) == 1)
+
+    # for each v in V setminus A there is exactly one edge adjacent to it
     for v in nodes:
         if v not in A:
-            m.addConstr(gurobipy.quicksum([edge_var for edge, edge_var in edges.items() if v in edge]) == 1)
-            
+            m.addConstr(quicksum([edge_var for edge, edge_var in edges.items() if v in edge]) == 1)
+
     # set optimisation objective: maximum/minimum weight matching (sum of weights of chosen edges)
-    m.setObjective(gurobipy.quicksum([edge_var * G.G.edges[edge][weight] for edge, edge_var in edges.items()]), direction)
-    
+    m.setObjective(quicksum([edge_var * G.G.edges[edge][weight] for edge, edge_var in edges.items()]), direction)
+
     return m
 
-def extractSolution(G, model):
+
+def extract_solution(G, model):
     """ Get a list of the edges comprising the miniumum/maximum weight perfect bipartite matching
-    
+
         :param G: a weighted :py:class:`~graphilp.imports.ilpgraph.ILPGraph`
         :param model: a solved Gurobi model for minimum/maximum weight perfect bipartite matching
-            
+
         :return: a list of edges comprising the minimum/maximum weight perfect bipartite matching
     """
     if model.Status == GRB.INFEASIBLE:
         matching = []
     else:
         matching = [edge for edge, edge_var in G.edge_variables.items() if edge_var.X > 0.5]
-    
+
     return matching
-
-
