f6a4805ad2ba4222efee65f9818f6663594167e4
Adrian <>
2021-05-26 14:04:49 +0200

    Add warmstart

--- graphilp/examples/test.py
+++ graphilp/examples/test.py
@@ -30,19 +30,11 @@ if __name__ == '__main__':
         G.add_edges_from([(1, 2, {'weight': 2}), (1, 4, {'weight': 5}), (2, 4, {'weight': 1}),
                           (3, 4, {'weight': 1}), (4, 5, {'weight': 9}), (3, 6, {'weight': 4})])
 
-        root = -1
-        G, forced_terminals, sum_of_profits, root = pu.pcst_to_rpcst(G)
-        for t in forced_terminals:
-            G.nodes[t]['prize'] = 1000000000
+        root = 1
 
-
-        vor.reductionTechniques(G, root)
-        print(G.edges)
-        for t in forced_terminals:
-            G.nodes[t]['prize'] = 0
         G = nx.to_undirected(G)
         optG = imp_nx.read(G)
-        m = pl.create_model(optG, forced_terminals=forced_terminals, weight='weight')
+        m = pl.create_model(optG, forced_terminals=[root], weight='weight')
         m.optimize()
         best_val = m.objVal
         solution = stp.extract_solution(optG, m)
@@ -58,6 +50,6 @@ if __name__ == '__main__':
         for u in G.nodes():
             if u not in res_nodes:
                 result += G.nodes[u]['prize']
-        result -= len(forced_terminals) * sum_of_profits
+        #result -= len(forced_terminals) * sum_of_profits
         print(result)
         print(solution)
