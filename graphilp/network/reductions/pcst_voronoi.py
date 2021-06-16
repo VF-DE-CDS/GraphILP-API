365c23af9f03881274102056e1dcc81bb3e3e3b8
Adrian <>
2021-06-16 14:13:40 +0200

    Fininsh docstrings, pep8 for Voronoi

--- graphilp/network/reductions/pcst_voronoi.py
+++ graphilp/network/reductions/pcst_voronoi.py
@@ -1,21 +1,22 @@
 """
-Geometric reduction technique to solve the PCST.
-The techniques come from the paper "Reduction techniques for the prize collecting Steiner tree problem
-and the maximum-weight connected subgraph problem"
-by Daniel Rehfeldt, Thorsten Koch and Stephen J. Maher from 2018
+Reduction technique that uses geometric properties of the underlying graph to solve the PCST.
+The techniques come from the paper:
+Rehfeldt, Daniel, Thorsten Koch, and Stephen J. Maher.
+"Reduction techniques for the prize collecting Steiner tree problem and the maximum‐weight connected subgraph problem."
+Networks 73.2 (2019): 206-233.
 """
 
 import networkx as nx
+from graphilp.network.reductions import pcst_basic_reductions as pcst_basic
 from graphilp.network.reductions import pcst_utilities as pcst_utilities
 
 
 def voronoi_diagram(G):
     """
     Divides the entire graph into different partitions, with one partition being created for each terminal.
-    Each node is assigned to the partition to which the associated terminal is closest.
-
+    Each non terminal node is assigned to the partition to which the associated terminal is closest.
     :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
-    :return: Dictionary with terminals as keys and associated nodes as values
+    :return: Dictionary with terminals as keys and associated nodes of the partition as values
     """
     terminals = pcst_utilities.compute_terminals(G)
     diagram = nx.voronoi_cells(G, terminals, weight='weight')
@@ -25,72 +26,80 @@ def voronoi_diagram(G):
     return diagram
 
 
-def pcradius(G, diagram):
+def pc_radius(G, diagram):
     """
-    Specifies for each partition the minimum cost of including the partition's terminal in the solution.
-    (Minimum from the prophet of the terminal and the minimum cost to reach the terminal from outside the partition.)
-
+    Specifies for each partition the minimum cost of the partition's terminal in the solution.
+    (Minimum from the profit of the terminal and the minimum cost to reach the terminal from outside the partition.)
     :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
     :param diagram: A dictionary of partitions of the graph
-    :return: Dictionary with terminals as keys and minimum costs as values
+    :return: Dictionary with terminals as keys and pc_radius as values
     """
-    radius = {}
+    pc_radius = {}
     for cell in diagram:
         profit = G.nodes[cell]['prize']
-        path = float('inf')
+        radius = float('inf')
         for node in diagram[cell]:
             neighbours = [n for n in G.neighbors(node) if n not in diagram[cell]]
             for neighbour in neighbours:
                 temp_path = nx.shortest_path_length(G, source=cell, target=neighbour, weight='weight')
-                path = min(path, temp_path)
-        radius[cell] = min(path, profit)
-    return radius
+                radius = min(radius, temp_path)
+        pc_radius[cell] = min(radius, profit)
+    return pc_radius
 
 
 def is_same_base(v, w, diagram):
     """
-    Checks whether two nodes w and v are in the same partition.
-
-    :param v: Integer representing node1
-    :param w: Integer representing node1
-    :param diagram: Dictionary representin partitions
+    Checks whether two nodes w and v are in the same voronoi partition.
+    :param v: Integer representing node 1
+    :param w: Integer representing node 2
+    :param diagram: Dictionary representing partitions
     :return: Boolean if the nodes are in the same partition
     """
     for terminal in diagram:
-        partiton = diagram[terminal]
-        if v in partiton and w in partiton:
+        partition = diagram[terminal]
+        if v in partition and w in partition:
             return True
     return False
 
 
-def proposition13(G, radius, upperBound):
-    """
-    Deletes all nodes with which the solution would be worse than the upper bound found with PCST-fast
+###############################################
+# The following are all the reduction techniques
+###############################################
 
+def proposition13(G, pc_radius, upperBound):
+    """
+    Deletes all non-terminal nodes with which the local lower bound would be worse than the global upper bound
     :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
-    :param radius: dictionary which saves the minimum cost of including a partition
-    :param upperBound: worst possible solution
+    :param pc_radius: list which saves the minimum cost of including a partition
+    :param upperBound: Global upper bound
     """
     terminals = pcst_utilities.compute_terminals(G)
     nodes = [v for v in G.nodes if v not in terminals]
     for node in nodes:
-        dNearestTerminal = pcst_utilities.d_nearest_terminals_root(G, node, terminals, numberOfNearestTerminals=2)
-        dNearestTerminal = list(dNearestTerminal.values())
-        lowerBound = sum(radius[0:-2]) + sum(dNearestTerminal)
+        d_nearest_terminal = pcst_utilities.d_nearest_terminals_root(G, node, terminals, numberOfNearestTerminals=2)
+        d_nearest_terminal = list(d_nearest_terminal.values())
+        lowerBound = sum(pc_radius[0:-2]) + sum(d_nearest_terminal)
         if round(lowerBound, 3) > round(upperBound, 3):
             G.remove_node(node)
 
 
-def corollary14(G, radius_list, radius_dict, upperBound):
+def corollary14(G, radius_dict, upperBound):
+    """
+    Deletes all terminal nodes with which the local lower bound would be worse than the global upper bound
+    :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
+    :param radius_dict: A dictionary with terminals as keys an the belonging pc_radius as values
+    :param upperBound: Global upper bound
+    """
     terminals = pcst_utilities.compute_terminals(G)
     terminals = [t for t in terminals]
     for t in terminals:
+        radius_list = list(radius_dict.values())
         radius_list.sort()
         radius_list.remove(radius_dict.get(t))
         radius_list.insert(0, radius_dict.get(t))
         dNearestTerminal = pcst_utilities.d_nearest_terminals(G, t, terminals, duin=False)
-        # Für den Fall, dass das Terminal mit keinem anderen Terminal verbunden ist kann ich das Terminal direkt
-        # löschen, da wir für das Korollar von min 2 Terminals ausgehen
+        # In the case that the terminal is not connected to any other terminal delete the terminal directly,
+        # because we assume min 2 terminals for the corollary
         if len(dNearestTerminal) == 0:
             G.remove_node(t)
         else:
@@ -99,22 +108,38 @@ def corollary14(G, radius_list, radius_dict, upperBound):
                 G.remove_node(t)
 
 
-def proposition15(G, radius, radius_dict, upperBound):
+def proposition15(G, radius_dict, upperBound):
+    """
+   Deletes all terminal nodes with which the local lower bound would be worse than the global upper bound.
+   This method is more specific than corollary 14.
+   :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
+       :param radius_dict: A dictionary with terminals as keys an the belonging pc_radius as values
+    :param upperBound: Global upper bound
+   """
     terminals = pcst_utilities.compute_terminals(G)
     term_deg2 = [t for t in terminals if G.degree[t] >= 2]
     res_nodes = []
     for t in term_deg2:
-        radius.remove(radius_dict.get(t))
-        radius.insert(0, radius_dict.get(t))
+        radius_list = list(radius_dict.values())
+        radius_list.sort()
+        radius_list.remove(radius_dict.get(t))
+        radius_list.insert(0, radius_dict.get(t))
         dNearestTerminal = pcst_utilities.d_nearest_terminals(G, t, terminals, 'weight', 2)
-        lowerBound = sum(radius[1:-2]) + sum(dNearestTerminal)
+        lowerBound = sum(radius_list[1:-2]) + sum(dNearestTerminal)
         if round(lowerBound, 3) > round(upperBound, 3):
             res_nodes.append(t)
-    radius.sort()
     return res_nodes
 
 
 def proposition17(G, radius, upperBound, diagram):
+    """
+    Deletes all edges with which the local lower bound would be worse than the global upper bound.
+    :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
+    :param radius: A list with terminals as keys an the belonging pc_radius as values
+    :param upperBound: Global upper bound
+    :param diagram: Dictionary representing partitions
+    """
+    radius.sort()
     terminals = pcst_utilities.compute_terminals(G)
     edges_to_remove = []
     for (v, w) in G.edges:
@@ -138,6 +163,13 @@ def proposition17(G, radius, upperBound, diagram):
 
 
 def proposition18(G, radius, upperBound):
+    """
+    Deletes all non terminals with degree >= 3 if the local lower bound would be worse than the global upper bound.
+    :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
+    :param radius: A list with terminals as keys an the belonging pc_radius as values
+    :param upperBound: Global upper bound
+    """
+    radius.sort()
     terminals = pcst_utilities.compute_terminals(G)
     nodes = [n for n in G.nodes if G.degree[n] >= 3]
     res_nodes = []
@@ -151,30 +183,27 @@ def proposition18(G, radius, upperBound):
 
 def reduction_techniques(G, root=-1):
     """
+    Routine for all the Voronoi diagram reduction tests
     :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
     :param root: integer representing the root of the graph if -1 there is no root
     :return:
     """
     # Use PCST-fast to compute an upper bound
     upperBound = pcst_utilities.compute_upper_bound(G, root)
-    # Partitioning the graph and computing the pcradius for each partition
+    # Partitioning the graph and computing the pc_radius for each partition
     diagram = voronoi_diagram(G)
-    radius_dict = pcradius(G, diagram)
+    radius_dict = pc_radius(G, diagram)
     radius_list = list(radius_dict.values())
     radius_list.sort()
     # Use the propositions and corollaries suggested in the underlying paper
     proposition13(G, radius_list, upperBound)
-    corollary14(G, radius_list, radius_dict, upperBound)
-    term_deg2 = proposition15(G, radius_list, radius_dict, upperBound)
+    corollary14(G, radius_dict, upperBound)
+    # TODO: The information is not used right now. Does it work with proposition 16?
+    # TODO: Don't understand proposition 16, is it useful with prop 15?
+    term_deg2 = proposition15(G, radius_dict, upperBound)
     proposition17(G, radius_list, upperBound, diagram)
+    # TODO: The information is not used right now.
     nodes_deg3 = proposition18(G, radius_list, upperBound)
-    # Delete components not connected to the root #TODO: Find solution
-    if root != -1:
-        nodes = []
-        for v in G.nodes:
-            try:
-                nx.shortest_path(G, root, v)
-            except nx.exception.NetworkXNoPath:
-                nodes.append(v)
-        G.remove_nodes_from(nodes)
+    # Delete components not connected to the root
+    pcst_basic.unconnected_component(G, root)
     return term_deg2, nodes_deg3
