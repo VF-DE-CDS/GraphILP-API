5fb7f5a0a6d73f3a552a93c90308ea9f98fa8e2e
Adrian <>
2021-06-16 08:52:36 +0200

    Finish docstrings, pep8

--- graphilp/network/reductions/pcst_basic_reductions.py
+++ graphilp/network/reductions/pcst_basic_reductions.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from graphilp.network.reductions import pcst_utilities
 
+
 # All reduction techniques described here are taken from the following paper:
 # REHFELDT, Daniel; KOCH, Thorsten; MAHER, Stephen J.
 # Reduction techniques for the prize collecting Steiner tree problem and the maximum‐weight connected subgraph problem.
@@ -12,7 +13,7 @@ def ntd1(G, terminals):
     :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
     :param terminals: A list of all terminals
     """
-    nodes_to_remove = [n[0] for n in G.nodes(data=True) if G.degree[n[0]] == 1 and n[0] not in terminals]
+    nodes_to_remove = [n for n in G.node if G.degree[n] == 1 and n not in terminals]
     G.remove_nodes_from(nodes_to_remove)
 
 
@@ -23,23 +24,23 @@ def ntd2(G, terminals):
     :param G: a `NetworkX graph  <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__ 
     :param terminals: A list of all terminals 
     """
-    nodes_to_remove = [n for n in G.nodes(data=True) if G.degree[n[0]] == 2 and n[0] not in terminals]
+    nodes_to_remove = [n for n in G.nodes if G.degree[n] == 2 and n not in terminals]
     for node in nodes_to_remove:
-        if len(G.adj[node[0]]) > 1:  # Sometimes adjacent nodes are already deleted, so that the node has only one
+        if len(G.adj[node]) > 1:  # Sometimes adjacent nodes are already deleted, so that the node has only one
             # neighbour left
-            u, v = list(G.neighbors(node[0]))[0], list(G.neighbors(node[0]))[1]
+            u, v = list(G.neighbors(node))[0], list(G.neighbors(node))[1]
             # To translate it back after computation
-            old_edges = G.edges(node[0], data=True)
+            old_edges = G.edges(node, data=True)
             new_path = []
             for e in old_edges:
                 if 'path' in e[2]:
                     new_path += e[2]['path']
                 else:
                     new_path.append(e[:2])
-            edge_length = G.get_edge_data(node[0], u).get('weight') + G.get_edge_data(node[0], v).get('weight')
+            edge_length = G.get_edge_data(node, u).get('weight') + G.get_edge_data(node, v).get('weight')
             if not G.has_edge(u, v) or G.has_edge(u, v) and G.get_edge_data(u, v).get('weight') > edge_length:
                 G.add_edge(u, v, weight=edge_length, path=new_path)
-        G.remove_node(node[0])
+        G.remove_node(node)
 
 
 def td1(G, terminals, root):
@@ -68,7 +69,6 @@ def td2(G, root):
     """
     Terminal of degree 2 can be substituted by an edge if its profit is too small to include the terminal-
     :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
-    :param terminals: A list of all terminals
     :param root: An integer representing the root
     """
     terminals = pcst_utilities.compute_terminals(G)
@@ -80,7 +80,7 @@ def td2(G, root):
         length_e1 = G.get_edge_data(n, neighbors[0])['weight']
         length_e2 = G.get_edge_data(n, neighbors[1])['weight']
         profit_terminals = [G.nodes[t]["prize"] for t in terminals if t != n]
-        # Um zurückübersetzen zu können
+        # To be able to translate it back
         old_edges = G.edges(n, data=True)
         new_path = []
         for e in old_edges:
@@ -95,10 +95,11 @@ def td2(G, root):
     G.remove_nodes_from(nodes_to_remove)
 
 
-def unconnected_component(G):
+def unconnected_component(G, root=None):
     """
     Delete any component that does not contain terminals.
     :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
+    :param root: An integer representing the root
     """
     terminals = pcst_utilities.compute_terminals(G)
     terminal_nodes = [t for t in terminals]
@@ -106,16 +107,25 @@ def unconnected_component(G):
         for comp in list(nx.connected_components(G)):
             delete_component = True
             for n in comp:
-                if n in terminal_nodes:
-                    delete_component = False
+                if root is None:
+                    if n in terminal_nodes:
+                        delete_component = False
+                else:
+                    if n == root:
+                        delete_component = False
             if delete_component:
                 [G.remove_node(n) for n in comp]
 
 
 def basic_reductions(G, root):
+    """
+    Calls all Basic Reductions one after the other
+    :param G: a `NetworkX graph <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`__
+    :param root: An integer representing the root
+    """
     terminals = pcst_utilities.compute_terminals(G)
     ntd1(G, terminals)
     ntd2(G, terminals)
     td1(G, terminals, root)
     td2(G, root)
-    unconnected_component(G)
+    unconnected_component(G, root)
